4317548a2f7cd2eeae53289b55f8a98605bb99fe
rayylee <rayylee@foxmail.com>
2024-04-29 23:50:40 +0800

    riscv: fix the timer and cpu frequency are not related
    Signed-off-by: rayylee <rayylee@foxmail.com>

--- modules/axhal/src/platform/riscv64_qemu_virt/mod.rs
+++ modules/axhal/src/platform/riscv64_qemu_virt/mod.rs
@@ -11,6 +11,8 @@ pub mod irq;
 #[cfg(feature = "smp")]
 pub mod mp;
 
+use of;
+
 extern "C" {
     fn trap_vector_base();
     fn rust_main(cpu_id: usize, dtb: usize);
@@ -18,10 +20,26 @@ extern "C" {
     fn rust_main_secondary(cpu_id: usize);
 }
 
+fn init_board_info(dtb: usize) {
+    unsafe {
+        of::init_fdt_ptr(dtb as *const u8);
+    }
+    let mut of_cpus = of::cpus();
+    let freq = {
+        if let Some(cpu) = of_cpus.nth(0) {
+            cpu.timebase_frequency()
+        } else {
+            axconfig::TIMER_FREQUENCY
+        }
+    };
+    self::time::init_cpu_freq(freq as u64);
+}
+
 unsafe extern "C" fn rust_entry(cpu_id: usize, dtb: usize) {
     crate::mem::clear_bss();
     crate::cpu::init_primary(cpu_id);
     crate::arch::set_trap_vector_base(trap_vector_base as usize);
+    init_board_info(dtb);
     rust_main(cpu_id, dtb);
 }
 
